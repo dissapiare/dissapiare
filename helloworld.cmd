c387d6956c2c26efb3f73ce87467c0e397236a05
dissapiare <105976954+dissapiare@users.noreply.github.com>
2022-05-21 15:47:12 +0300

    Create helloworld.cmd

---
+++ helloworld.cmd
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @dissapiare
+- 👀 I’m interested in road construction and design
+- 🌱 I’m currently learning rejuvenation of bitumen structures and soil stabilizing
+- 💞️ I’m looking to collaborate on any interesting projecets that could let me explore something new
+- 📫 How to reach me dissapiare@gmail.com
+
+<!---
+dissapiare/dissapiare is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
